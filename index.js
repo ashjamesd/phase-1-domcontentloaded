954113b20c0d64f420b240b6217e8add6990de5d
Ash <ashjames.dean@gmail.com>
2023-01-16 15:10:42 -0500

    commit tree 70dfa9ec63e13a8d9cbac2cb6b4b27a6ace3c0cb

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+function textReplace(){
+    textP = document.getElementById('text').textContent="This is really cool!";
+    textP();
+}
+
+document.addEventListener("DOMContentLoaded",textReplace);
